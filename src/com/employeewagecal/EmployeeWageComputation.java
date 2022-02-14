db8a539d9c09fbd46984d92fcb96119cbaa4ea9f
first_name <ss329222@gmail.com>
2022-02-14 11:53:03 +0530

    Added:CheckEmployeePrescence

---
+++ src/com/employeewagecal/EmployeeWageComputation.java
@@ -0,0 +1,22 @@
+package com.employeewagecal;
+
+import java.util.Random;
+
+public class EmployeeWageComputation {
+	static final int IS_FULL_TIME = 1;
+
+	public static void empCheckPrescence() {
+		Random rand = new Random();
+		int empCheck = rand.nextInt(2);
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is absent");
+		}
+	}
+
+	public static void main(String[] args) {
+		empCheckPrescence();
+	}
+
+}
