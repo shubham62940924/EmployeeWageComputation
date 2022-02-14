a57037637dba3b27080094fdddb21f9b0dc717bc
first_name <ss329222@gmail.com>
2022-02-14 11:57:02 +0530

    Added:EmpPrescenceCheck

--- src/com/employeewagecal/EmployeeWageComputation.java
+++ src/com/employeewagecal/EmployeeWageComputation.java
@@ -11,7 +11,7 @@ public class EmployeeWageComputation {
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
 		} else {
-			System.out.println("Employee is absent");
+			System.out.println("Employee is absent ");
 		}
 	}
 
