e811f3fd5bb3779e1fe9d924995389aa363fb6c0
first_name <ss329222@gmail.com>
2022-02-14 13:24:32 +0530

    Added:TotalWorkingHours

--- src/com/employeewagecal/EmployeeWageComputation.java
+++ src/com/employeewagecal/EmployeeWageComputation.java
@@ -7,6 +7,7 @@ public class EmployeeWageComputation {
 	static final int IS_PART_TIME = 2;
 	static final int EMPLOYEE_RATE_PER_HOUR = 20;
 	static final int NUMBER_OF_WORKING_DAYS = 20;
+	static final int TOTAL_WORKING_HOURS = 100;
 
 	public static int empCheckWage() {
 		int empWorkingHours = 0;
@@ -30,21 +31,23 @@ public class EmployeeWageComputation {
 		return empWorkingHours;
 	}
 
-	public static int empWagePerDayStatus(int empWorkingHours) {
+	public static int empWagePerDay(int empWorkingHours) {
 		int empWagePerDay = empWorkingHours * EMPLOYEE_RATE_PER_HOUR;
 		return empWagePerDay;
 	}
 
 	public static void main(String[] args) {
 		int empWorkingHours;
+		int TotalWorkingHours = 0;
 		int empWagePerDay;
 		int days = 0;
 		int EmployeeMonthlyWage = 0;
-		while (days < NUMBER_OF_WORKING_DAYS) {
+		while (days < NUMBER_OF_WORKING_DAYS && TotalWorkingHours < TOTAL_WORKING_HOURS) {
 			days++;
 
 			empWorkingHours = empCheckWage();
-			empWagePerDay = empWagePerDayStatus(empWorkingHours);
+			TotalWorkingHours += empWorkingHours;
+			empWagePerDay = empWagePerDay(empWorkingHours);
 			EmployeeMonthlyWage += empWagePerDay;
 		}
 		System.out.println("Employee Wage per month: " + EmployeeMonthlyWage);
